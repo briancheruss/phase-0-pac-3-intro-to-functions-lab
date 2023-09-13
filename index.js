1b41207c0ada340d95ad29552c34bd8c5fed0454
briancherus <brian.cherus@student.moringaschool.comssh-keygen -t rsa -b 4096 -C brian.cherus@student.moringaschool.com>
2023-09-13 21:12:35 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+function logShout(string) {
+    const shoutedString = string.toUpperCase(); 
+    console.log(shoutedString); 
+  }
+
+  function logWhisper(string) {
+    const whisperedString = string.toLowerCase(); 
+    console.log(whisperedString); 
+  }
+
+  
+function sayHiToHeadphonedRoommate(string) {
+    if (string === string.toLowerCase()) {
+      return "I can't hear you!";
+    } else if (string === string.toUpperCase()) {
+      return "YES INDEED!";
+    } else if (string === "Let's have dinner together!") {
+      return "I would love to!";
+    }
+  }
